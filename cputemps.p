87419828de1b417518020b8fa2c70a59f20ffa2f
Jayson Pye <s3819431@student.rmit.edu.au>
2021-11-20 17:27:35 +1000

    Added signal handling to profiler.sh script. GNUPlot scripts testing for cputemps.

---
+++ cputemps.p
@@ -0,0 +1,10 @@
+#output to png file
+set terminal png size 700,700
+set output 'cputemps.png'
+#Set title of the graphic
+set title 'Cpu temperatures during compilation'
+set xlabel "Seconds"
+set ylabel "CPU temperature"
+#plot graphic
+plot "cputemps.plottieboy"
+
--- profiler.sh
+++ profiler.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
+
+
 SECONDS=0
-#Remove info from previously generated files
 #Begin timing loop
+#Function to be used in order to finalize data, call GNUplot scripts and exit gracefully.
+function plotAndExit()
+{
+	paste - - <cputemps.txt > cputemps.dat
+	paste - - <cpufrequency.txt > cpufrequency.dat
+	paste - - <gputemps.txt > gputemps.dat
+	paste - - <usedmemory.txt > usedmemory.dat
+	paste - - <freememory.txt > freememory.dat
+	paste - - <buffcachememory.txt > buffcachememory.dat
+	paste - - <availablememory.txt > availablememory.dat
+	paste - - <cpuusage.txt > cpuusage.dat
+	echo "\n"
+	echo "Plotting graphs and exiting."
+	exit 1
+}
+#Trap to be used for SIGINT signal to call exit function.
+trap 'plotAndExit' SIGINT
+
+#Remove old txt and dat files if they exist so that data isnt appended
+rm cputemps.txt gputemps.txt usedmemory.txt freememory.txt buffcachememory.txt availablememory.txt cpufrequency.txt cpuusage.txt 2> /dev/null
+rm cputemps.dat gputemps.dat usedmemory.dat freememory.dat buffcachememory.dat availablememory.dat cpufrequency.dat cpuusage.dat 2>/dev/null
+
+#Begin the loop to obtain data
 while true
 do
 	#Save seconds to txt files
@@ -13,15 +37,16 @@ do
 	#Edit data and save CPU frequency to a file
 	frequency=$(vcgencmd measure_clock arm)
 	echo $frequency | awk -F= '{printf ("%.2f \n", ($2/1000000000))}' >> cpufrequency.txt
+
+	#Save GPU temp to a file
+	vcgencmd measure_temp | awk -F= '{printf("%.2f \n", ($2))}' >> gputemps.txt
 	
 	#Obtain memory stats and save to various files
 
-	free | awk 'NR==2 {printf ("%.2f:%.2f:%.2f:%.2f:%.2f:%.2f:%.2f \n", "($2/1000)""($3/1000)""($4/1000)""($5/1000)""($6/1000)""($7/1000)")}'
-	
-	echo $memory | awk '{printf ("%.2f \n", ($9/1000))}' >> usedmemory.txt
-	echo $memory | awk '{printf ("%.2f \n", ($10/1000))}' >> freememory.txt
-	echo $memory | awk '{printf ("%.2f \n", ($12/1000))}' >> buffcachememory.txt
-	free | awk '{printf ("%.2f \n", ($13/1000))}' >> availablememory.txt
+	free | awk 'NR==2 {printf ("%.2f \n", ($3/1000))}' >> usedmemory.txt
+	free | awk 'NR==2 {printf ("%.2f \n", ($4/1000))}' >> freememory.txt
+	free | awk 'NR==2 {printf ("%.2f \n", ($6/1000))}' >> buffcachememory.txt
+	free | awk 'NR==2 {printf ("%.2f \n", ($7/1000))}' >> availablememory.txt
 
 	#save CPU usage
 	mpstat | awk 'NR==4 {print $3, $4, $5, $6, $7, $8, $9, $10, $11, $12}' >> cpuusage.txt
@@ -29,3 +54,4 @@ do
 	#Free up the CPU for 1 second until writing begins again
 	sleep 1
 done
+
